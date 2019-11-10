043e95f9b03ac662df7af11749c762dea883ee46
Andrés Boc-Ho <andresin@gmail.com>
2019-11-10 19:48:21 -0300

    feature: display playlist mode in yellow

--- options.js
+++ options.js
@@ -8,24 +8,24 @@ let page = document.getElementById('configDiv');
 
 
 function constructOptions() {
-  let inputText = document.createElement('input');
-  inputText.type = 'text';
-  chrome.storage.sync.get(['apiURL'], (result) => {
-    document.getElementById('valorActual').innerText = result.apiURL;
-    inputText.value = result.apiURL;
-  });
-
-  let button = document.createElement('button');
-  button.innerText = 'Guardar'
-  button.className = 'btn-lantube';
-  button.addEventListener('click', (ev) => {
-    ev.preventDefault();
-    chrome.storage.sync.set({ apiURL: inputText.value }, () => {
-      console.log('New API URL is ' + inputText.value);
-      document.getElementById('valorActual').innerText = inputText.value;
+    let inputText = document.createElement('input');
+    inputText.type = 'text';
+    chrome.storage.sync.get(['apiURL'], (result) => {
+        document.getElementById('valorActual').innerText = result.apiURL;
+        inputText.value = result.apiURL;
     });
-  });
-  page.appendChild(inputText);
-  page.appendChild(button);
+
+    let button = document.createElement('button');
+    button.innerText = 'Guardar'
+    button.className = 'btn-lantube';
+    button.addEventListener('click', (ev) => {
+        ev.preventDefault();
+        chrome.storage.sync.set({ apiURL: inputText.value }, () => {
+            console.log('New API URL is ' + inputText.value);
+            document.getElementById('valorActual').innerText = inputText.value;
+        });
+    });
+    page.appendChild(inputText);
+    page.appendChild(button);
 }
 constructOptions();
--- popup.js
+++ popup.js
@@ -4,24 +4,29 @@ let page = document.getElementById('videos');
 let lista = document.createElement('ol');
 
 function constructOptions(apiURL, videos) {
+
     for (let video of videos) {
 
         let title = document.createElement('li');
-        title.innerText = video.videoInfo.title;
-        title.id = video.videoInfo.videoId;
-
         let order = document.createElement('b');
+        let play = document.createElement('a');
+
+        let videoTitle = video.videoInfo ? video.videoInfo.title : video.title;
+        let videoId = video.videoInfo ? video.videoInfo.videoId : video.videoId;
+
+        title.innerText = videoTitle;
+        title.id = videoId;
+
         order.innerText = video.order;
 
-        let play = document.createElement('a');
         play.innerText = '▶'; // ‣
         play.addEventListener('click', function () {
             let self = this;
-            let playVideo = confirm(`¿Reproducir ${video.videoInfo.title}?`);
+            let playVideo = confirm(`¿Reproducir ${videoTitle}?`);
             if (playVideo) {
                 fetch(`${apiURL}api/player/stop`).then((stopped) => {
                     if (stopped.ok) {
-                        fetch(`${apiURL}api/player/${video.videoInfo.videoId}/play`).then((response) => {
+                        fetch(`${apiURL}api/player/${videoId}/play`).then((response) => {
                             if (response.ok) {
                                 response.json().then(playing => {
                                     self.classList.add('playing');
@@ -104,6 +109,14 @@ chrome.storage.sync.get(['apiURL'], (result) => {
     fetch(`${result.apiURL}api/player/stats`).then((stats) => {
         if (stats.ok) {
             stats.json().then(st => {
+
+                let playlist = document.getElementById('playlist');
+                if (st.playlist) {
+                    playlist.classList.add('playlist');
+                } else {
+                    playlist.classList.remove('playlist');
+                }
+
                 // PAUSE
                 if (st.status === 'paused') {
                     let pause = document.getElementById('pause');
@@ -112,14 +125,17 @@ chrome.storage.sync.get(['apiURL'], (result) => {
                 fetch(`${result.apiURL}api/videos`).then((response) => {
                     if (response.ok) {
                         response.json().then(videos => {
-                            constructOptions(result.apiURL, videos);
-                            // CURRENT
-                            if (st.status === 'playing') {
-                                console.log(st.videoId);
-                                let playing = document.getElementById(st.videoId);
-                                playing.classList.add('playing');
-                                let pause = document.getElementById('pause');
-                                pause.classList.remove('paused');
+                            if (videos.length > 0) {
+                                constructOptions(result.apiURL, videos.reverse());
+                                // CURRENT
+                                if (st.status === 'playing') {
+                                    let playing = document.getElementById(st.videoId);
+                                    playing.classList.add('playing');
+                                    let pause = document.getElementById('pause');
+                                    pause.classList.remove('paused');
+                                }
+                            } else {
+                                document.getElementById('videos').innerHTML = '<h4>No hay videos en la lista</h4>';
                             }
                         });
                     }
