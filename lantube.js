ec824b088c1a6c60342c17b8230dbf6de935e58d
Andrés Boc-Ho <andresin@gmail.com>
2019-11-30 11:06:07 -0300

    feature: add buttons after scroll lazy load of videos

--- lantube.js
+++ lantube.js
@@ -1,27 +1,43 @@
 setTimeout(() => {
-    let buttons = [];
-
-    let itemCount = document.querySelectorAll('a[id=video-title]').length;
-
-    // function nodeInsertedCallback(event) {
-    //     setTimeout(() => {
-    //         let eventItemCount = document.querySelectorAll('a[id=video-title]').length;
-    //         console.log({ eventItemCount });
-    //         if (itemCount < eventItemCount) {
-    //             console.log({ event });
-    //             document.querySelectorAll('.btn-lantube').forEach((item) => item.remove());
-    //             addLantubeButtons();
-    //             eventItemCount = 0;
-    //         }
-    //     }, 4000)
-    // };
 
     function addLantubeButtons() {
 
-        document.querySelectorAll('a[id=video-title]').forEach((item, key) => {
+        let buttons = [];
+
+        // Select the node that will be observed for mutations
+        const targetNode = document.getElementById('contents');
+
+        // Options for the observer (which mutations to observe)
+        const config = { attributes: true, childList: true, subtree: false };
+
+        // Callback function to execute when mutations are observed
+        const callback = function (mutationsList, observer) {
+            // Use traditional 'for loops' for IE 11
+            for (let mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    Array.from(document.getElementsByClassName('btn-lantube')).map(x => x.remove());
+                    addLantubeButtons();
+                }
+                else if (mutation.type === 'attributes') {
+                    console.log('The ' + mutation.attributeName + ' attribute was modified.');
+                }
+            }
+        };
+
+        // Create an observer instance linked to the callback function
+        const observer = new MutationObserver(callback);
+
+        // Start observing the target node for configured mutations
+        observer.observe(targetNode, config);
+
+        // Later, you can stop observing
+        // observer.disconnect();
+
+
+        document.querySelectorAll('a[id=video-title],span[id=video-title],a[id=wc-endpoint]').forEach((item, key) => {
 
             function addVideo() {
-                let ytid = item.href.trim().replace(/http(s?):\/\/(w{3}?)(\.?)youtube\.com\/watch\?v=/, '');
+                let ytid = item.href.trim().replace(/&list=(.*)/, '').replace(/http(s?):\/\/(w{3}?)(\.?)youtube\.com\/watch\?v=/, '');
                 console.log(ytid);
 
                 if (confirm('¿Agregar a Lantube?')) {
@@ -35,10 +51,6 @@ setTimeout(() => {
 
                     });
 
-                    // buttons[key].innerText = '✕';
-                    // buttons[key].classList.remove('btn-lantube');
-                    // buttons[key].className = 'btn-lantube-disabled';
-                    // buttons[key].removeEventListener('click', addVideo, false);
                 }
             }
 
@@ -55,4 +67,4 @@ setTimeout(() => {
 
     addLantubeButtons();
 
-}, 2000);
+}, 3000);
